4ecb17929468bed4ccb04282fe81f61ee2c48206
Minidu-Sathsara <138673580+Minidu-Sathsara@users.noreply.github.com>
2024-06-24 14:25:09 +0530

    add table

---
+++ app.js
@@ -0,0 +1,7 @@
+const sliderContainer= document.querySelector(".slider-container");
+const salidRight= document. querySelector(".right-slid"); 
+const salidleft =document. querySelector(".left-slid");
+const upButton= document. getElementById(".up-btn");
+const downButton= document. getElementById(".down-btn");
+const saliderLength= slideRight. querySelectorAll(".div").length;
+
